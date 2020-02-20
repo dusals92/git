db54c29ba3759114e2338e119ff92e5e8ec41300
DREAM-06\7010master <dusals_92@naver.com>
2020-02-20 12:04:32 +0900

    수정1204

--- 03-1/03-1/ex1.py
+++ 03-1/03-1/ex1.py
@@ -2,7 +2,9 @@ data = input("Enter list of numbers: ")
 numbers = data.split()
 numbers = [int(i) for i in numbers]
 #minval = 99999999
-minval = 12345678
+#minval = 12345678
+#minval = 1234567890
+minval = 12345
 for val in numbers:
     if (minval > val):
         minval = val
